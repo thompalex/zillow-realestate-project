1bc662d9936abcf44f517fc1c8970605978befe7
Alex <athomps3@oberlin.edu>
2022-12-11 19:40:26 -0500

    organized data files

--- data_work.py
+++ data_work.py
@@ -6,7 +6,7 @@ import os
 
 # This is where the actual data stuff is done for the most part
 def get_unique_neighborhoods():
-    dataset = pd.read_csv("project_dataset.csv")
+    dataset = pd.read_csv("data/project_dataset.csv")
     unique_neighborhoods = set()
     df_with_unique = pd.DataFrame(columns=['Neighborhood', 'City', 'State'])
     length = len(df_with_unique)
@@ -15,7 +15,7 @@ def get_unique_neighborhoods():
             unique_neighborhoods.add(t[1])
             df_with_unique.loc[length] = [t[2], t[4], t[9]]
             length += 1
-    df_with_unique.to_csv("unique_neighborhoods.csv")
+    df_with_unique.to_csv("data/other/unique_neighborhoods.csv")
 
 def add_latlon():
     conn = http.client.HTTPConnection('api.positionstack.com')
@@ -36,9 +36,9 @@ def add_latlon():
             return f'{api_result["data"][0]["latitude"]}, {api_result["data"][0]["longitude"]}'
         else:
             return ""
-    unique_neighborhoods = pd.read_csv('unique_neighborhoods.csv')
+    unique_neighborhoods = pd.read_csv('data/other/unique_neighborhoods.csv')
     unique_neighborhoods['latlon'] = unique_neighborhoods.apply(lambda x: get_geocode(x['Neighborhood'], x['City'], x['State']), axis=1)
-    unique_neighborhoods.to_csv('unique_neighborhoods_with_latlon.csv')
+    unique_neighborhoods.to_csv('data/other/unique_neighborhoods_with_latlon.csv')
 
 
 
@@ -62,18 +62,18 @@ def add_zipcodes():
                 return ""
         except:
             return ""
-    unique_neighborhoods_w_latlon = pd.read_csv('unique_neighborhoods_with_latlon.csv')
+    unique_neighborhoods_w_latlon = pd.read_csv('data/other/unique_neighborhoods_with_latlon.csv')
     unique_neighborhoods_w_latlon['zipcode'] = unique_neighborhoods_w_latlon.apply(lambda x: get_zipcode(x['latlon']), axis=1)
-    unique_neighborhoods_w_latlon.to_csv('unique_neighborhoods_w_zip_latlon.csv')
+    unique_neighborhoods_w_latlon.to_csv('data/other/unique_neighborhoods_w_zip_latlon.csv')
 
 
 # Generate the project nd from the zillow files and the region mapping
 def generate_dataset():
-    region_mapping = pd.read_csv('states.csv')
+    region_mapping = pd.read_csv('data/other/states.csv')
     # Concatenate each of the csvs that correspond to individual bedroom amounts
     arr = []
     for i in range(1, 6):
-        nd = pd.read_csv(f'zillow_data/{i}_bedroom.csv')
+        nd = pd.read_csv(f'data/zillow_data/{i}_bedroom.csv')
         nd = nd[['RegionID', 'RegionName', 'State', 'City', 'Metro', '2022-10-31']]
         nd['bedrooms'] = i
         arr.append(nd)
@@ -85,7 +85,7 @@ def generate_dataset():
     # Reset the indicies and save the DF to a csv
     data_with_region.reset_index()
     # Open Neighborhood to latlon and zipcode mapping file
-    zip_latlon_mapping = pd.read_csv('unique_neighborhoods_w_zip_latlon.csv')
+    zip_latlon_mapping = pd.read_csv('data/other/unique_neighborhoods_w_zip_latlon.csv')
     # Merge zipcodes and latlons with zillow data
     data_w_zip_latlon = pd.merge(zip_latlon_mapping, data_with_region, left_on=['Neighborhood', 'State', 'City'], right_on=['RegionName', 'State Name', 'City'])
     # Format data nicely
@@ -93,7 +93,7 @@ def generate_dataset():
     data_w_zip_latlon['Price'] = data_w_zip_latlon['2022-10-31']
     data_w_zip_latlon = data_w_zip_latlon[['Neighborhood','latlon','zipcode','RegionID','State','City','Metro','Price','bedrooms','Region','State Name']]
     # Load in price forecasts
-    price_forecasts = pd.read_csv('zillow_data/pricing_forecast.csv')    
+    price_forecasts = pd.read_csv('data/zillow_data/pricing_forecast.csv')    
     price_forecasts = price_forecasts[['RegionName', '2023-10-31']]
     # Merge price forecasts into project dataset
     complete_df = pd.merge(data_w_zip_latlon, price_forecasts, left_on='zipcode', right_on='RegionName', how='left')
@@ -102,16 +102,16 @@ def generate_dataset():
     complete_df['zipcode'] = complete_df['RegionName']
     complete_df['forecast'] = complete_df['2023-10-31']
     complete_df = complete_df[['Neighborhood','latlon','zipcode','RegionID','State','City','Metro','Price','bedrooms','Region','State Name', 'forecast']]
-    complete_df.to_csv('project_dataset.csv', index=False)
+    complete_df.to_csv('data/project_dataset.csv', index=False)
 
 
 # Make a query from the frontend
 def make_query(args):
     # If the necessary nd has not been generated yet, create it
-    if not os.path.exists('project_dataset.csv'):
+    if not os.path.exists('data/project_dataset.csv'):
         generate_dataset()
     # Read in the compiled data
-    data = pd.read_csv('project_dataset.csv').fillna(0)
+    data = pd.read_csv('data/project_dataset.csv').fillna(0)
     # Each of the four following lines filters out results that do not adhere to the inputs
     with_region = data[data['Region'] == args['region']]
     with_bedrooms = with_region[with_region['bedrooms'] == int(args['beds'])]
@@ -139,13 +139,14 @@ def make_query(args):
 
 ### Test statements for these methods ###
 if __name__ == '__main__':
-    generate_dataset() if not os.path.exists('project_dataset.csv') else None
+    generate_dataset() if not os.path.exists('data/project_dataset.csv') else None
     result = make_query({
             'region': 'Northeast',
             'beds': "1",
             'price': '800,000 - $1,000,000',
             'rate': '4% - 5%',
-            'timeline': '30 years'
+            'timeline': '30 years',
+            'forecast': True
         })
 
     print(result)
